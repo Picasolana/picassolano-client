57564a94c08339daff1366ca5a0b19c8f7ef746c
Bakhtilar Muzakparov <bakhtilar.muzakparov@UKHOITMB3824.local>
2024-03-09 14:23:29 +0000

    add logo to leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Nav } from "./Nav";
+import { Logo } from "./Logo";
 
 export const Leaderboard = () => {
   return (
     <main style={{ width: "100vw", height: "100vh" }}>
+      <Logo />
       <Nav />
       <div style={{ display: "flex", justifyContent: "center" }}>
         Leaderboard
